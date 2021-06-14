1f3929a6ada75e25bc4d8a6e17dfdf70bbbcadc9
Ananth Jonnavittula <ajonnavittula@wpi.edu>
2021-06-14 18:35:29 -0400

    partially working 2d

--- 2d_world.py
+++ 2d_world.py
@@ -47,9 +47,13 @@ class TrajOpt(object):
                 self.xi0[goal, waypoint, :] = self.home + waypoint /(self.n_waypoints - 1.0)\
                                                  * (self.goalset[goal] - self.home) 
         self.optimize()
+
     """ Find most likely goal using Bayesian Inference """
     def predict(self, pos):
-        return np.argmax(np.linalg.norm(self.goalset - pos))
+        dists = np.linalg.norm(self.goalset - pos, axis=1)
+        idx = np.argmax(dists)
+        print(idx)
+        return idx, dists[idx]/sum(dists)
 
     """ problem specific cost function """
     def trajcost(self, xi):
@@ -63,20 +67,20 @@ class TrajOpt(object):
             point = xi[idx]
             costs = []
             for i in range(len(self.goalset)):
-                costs.append(np.exp(-np.linalg.norm(self.goalset[i] - point)))
-            cost_state = 1 - costs[self.goal_idx] / np.sum(costs)
+                costs.append(-np.exp(np.linalg.norm(self.goalset[i] - point)))
+            cost_state = costs[self.goal_idx] / np.sum(costs)
             cost_total += cost_state * cost_scale
             dist2goal += np.linalg.norm(self.goalset[self.goal_idx] - point)
             if cost_scale > 1:
                 cost_scale = len(xi) - idx
             smoothcost_xi += np.linalg.norm(xi[idx,:] - xi[idx-1,:])**2
         # perhaps you have some overall trajectory cost
-        cost_total += 10 * smoothcost_xi
+        cost_total += 15 * smoothcost_xi
         cost_total += 0.1 * dist2goal
         return cost_total
 
     """ limit the actions to the given action set for trajectory"""
-    def action_constraint(self, xi):
+    def action_cons(self, xi):
         xi = xi.reshape(self.n_waypoints,self.n_joints)
         length = 0
         prev_point = xi[0, :]
@@ -90,29 +94,24 @@ class TrajOpt(object):
                     
         return self.action_limit - max_diff
 
-    def cardinal_cons(self, xi):
+    def start_cons(self, xi):
         xi = xi.reshape(self.n_waypoints,self.n_joints)
         start = xi[0,:]
         end = xi[-1, :]
+        return np.linalg.norm(start - self.home)
 
-        return np.linalg.norm(start - self.home) + \
-                np.linalg.norm(end - self.goalset[self.goal_idx])
-
-    """ Check if traj update required """
-    def update(self):
-        update = False
-        if self.goal is None:
-            self.goal = goal[self.predict()]
-        else:
-            idx = self.predict()
-            if not self.goal_idx == idx:
-                self.goal_idx = idx
+    def end_cons(self, xi):
+        xi = xi.reshape(self.n_waypoints,self.n_joints)
+        start = xi[0,:]
+        end = xi[-1, :]
+        return np.linalg.norm(end - self.goalset[self.goal_idx])
 
     """ use scipy optimizer to get optimal trajectory """
     def optimize(self, method='SLSQP'):
 
-        cons = [{'type': 'eq', 'fun': self.cardinal_cons},
-        {'type': 'ineq', 'fun': self.action_constraint}]
+        cons = [{'type': 'eq', 'fun': self.start_cons},
+        {'type': 'eq', 'fun': self.end_cons},
+        {'type': 'ineq', 'fun': self.action_cons}]
         for idx in range(len(self.goalset)):
             print("Optimizing goal {}".format(idx))
             self.goal_idx = idx
@@ -122,6 +121,16 @@ class TrajOpt(object):
             res = minimize(self.trajcost, xi0, method=method, constraints=cons)
             xi = res.x.reshape(self.n_waypoints,self.n_joints)
             self.xi0[idx, :, :] = xi
+
+    """ P control to nudge user towards legible trajectory """
+    def robot_action(self, pos, t):
+        idx, conf = self.predict(pos)
+        try:
+            req_pos = self.xi0[idx, t, :]
+        except IndexError:
+            req_pos = self.xi0[idx, self.n_waypoints-1, :]
+        return req_pos - pos, conf
+
 
     """ Plot legible trajectories """
     def plot(self):
@@ -178,86 +187,97 @@ def main():
     filename = sys.argv[1]
     savename = "data/demos/" + filename + ".pkl"
 
-    clock = pygame.time.Clock()
-    # pygame.init()
-    fps = 30
-
-    # world = pygame.display.set_mode([700, 700])
     position_player = np.asarray([.1, .35])
-    postition_blue = np.asarray([0.8, 0.4])
-    postition_green = np.asarray([0.8, 0.3])
+    postition_blue = np.asarray([0.8, 0.5])
+    postition_green = np.asarray([0.8, 0.2])
     postition_gray = np.asarray([0., 0.])
     obs_position = postition_blue.tolist() + postition_green.tolist() + postition_gray.tolist()
 
     opt = TrajOpt(position_player, [postition_blue, postition_green])
-    # print(opt.home)
-    # print(opt.xi0)
-    opt.plot()
+    P_gain = 0.1
 
-    # player = Player(position_player)
-    # blue = Object(postition_blue, [0, 0, 255])
-    # green = Object(postition_green, [0, 255, 0])
-    # gray = Object(postition_gray, [128, 128, 128])
+    plot = False
+    if plot:
+        opt.plot()
+    else:
+        clock = pygame.time.Clock()
+        pygame.init()
+        fps = 30
 
-    # sprite_list = pygame.sprite.Group()
-    # sprite_list.add(player)
-    # sprite_list.add(blue)
-    # sprite_list.add(green)
-    # sprite_list.add(gray)
+        world = pygame.display.set_mode([700, 700])
 
-    # world.fill((0,0,0))
-    # sprite_list.draw(world)
-    # pygame.display.flip()
-    # clock.tick(fps)
+        player = Player(position_player)
+        blue = Object(postition_blue, [0, 0, 255])
+        green = Object(postition_green, [0, 255, 0])
+        gray = Object(postition_gray, [128, 128, 128])
 
-    # demonstration = []
-    # sampletime = 5
-    # count = 0
+        sprite_list = pygame.sprite.Group()
+        sprite_list.add(player)
+        sprite_list.add(blue)
+        sprite_list.add(green)
+        sprite_list.add(gray)
 
-    # while True:
+        world.fill((0,0,0))
+        sprite_list.draw(world)
+        pygame.display.flip()
+        clock.tick(fps)
 
-    #     q = np.asarray([player.x, player.y])
-    #     # s = obs_position + q.tolist()
-    #     s = q.tolist()
+        demonstration = []
+        sampletime = 5
+        count = 0
+        t = 0
+        while True:
+            q = np.asarray([player.x, player.y])
+            # s = obs_position + q.tolist()
+            s = q.tolist()
 
-    #     stop = 0
-    #     z1 = 0
-    #     z2 = 0
-    #     action = [0, 0]
-    #     left = pygame.key.get_pressed()[pygame.K_LEFT]
-    #     up = pygame.key.get_pressed()[pygame.K_UP]
-    #     down = pygame.key.get_pressed()[pygame.K_DOWN]
-    #     right = pygame.key.get_pressed()[pygame.K_RIGHT]
-    #     stop = pygame.key.get_pressed()[pygame.K_RETURN]
-        
-    #     z1 = right - left
-    #     z2 = down - up
+            stop = 0
+            z1 = 0
+            z2 = 0
+            action = [0, 0]
+            left = pygame.key.get_pressed()[pygame.K_LEFT]
+            up = pygame.key.get_pressed()[pygame.K_UP]
+            down = pygame.key.get_pressed()[pygame.K_DOWN]
+            right = pygame.key.get_pressed()[pygame.K_RIGHT]
+            stop = pygame.key.get_pressed()[pygame.K_RETURN]
+            
+            z1 = right - left
+            z2 = down - up
 
-    #     action = [z1, z2]
+            a_h = 0.01 * np.asarray([z1, z2])
+            conf = 0.
+            a_r = np.asarray([0, 0])
+            # Get robot action only if human acts
+            if not sum(a_h) == 0:
+                if count % 10:
+                    t += 1
+                a_r, conf = opt.robot_action(q, t)
+                a_r = np.clip(a_r, -0.05, 0.05)
 
-    #     if stop:
-    #         pickle.dump( demonstration, open( savename, "wb" ) )
-    #         print(demonstration)
-    #         print("[*] Done!")
-    #         print("[*] I recorded this many datapoints: ", len(demonstration))
-    #         pygame.quit(); sys.exit()
+            if stop:
+                pickle.dump( demonstration, open( savename, "wb" ) )
+                print(demonstration)
+                print("[*] Done!")
+                print("[*] I recorded this many datapoints: ", len(demonstration))
+                pygame.quit(); sys.exit()
 
-    #     q += np.asarray(action) * 0.01
+            action = a_h + P_gain * a_r
+            q += action
 
-    #     # dynamics
-    #     player.update(q)
+            # dynamics
+            player.update(q)
 
-    #     # animate
-    #     world.fill((0,0,0))
-    #     sprite_list.draw(world)
-    #     pygame.display.flip()
-    #     clock.tick(fps)
+            # animate
+            world.fill((0,0,0))
+            sprite_list.draw(world)
+            pygame.display.flip()
+            clock.tick(fps)
 
-    #     pygame.event.pump()
-    #     # save
-    #     if not count % sampletime:
-    #         demonstration.append(s)
-    #     count += 1
+            pygame.event.pump()
+            # save
+            if not count % sampletime:
+                demonstration.append(s)
+            count += 1
 
 if __name__ == "__main__":
     main()
